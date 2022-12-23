65128a1cc7165a3375f9ec705bf6cc2047827ece
Sergei Nikulin <nikulin-91@mail.ru>
2022-12-23 21:52:09 +0400

    add docxTest

--- src/test/java/com/nikulin/tests/SelenideFilesTest.java
+++ src/test/java/com/nikulin/tests/SelenideFilesTest.java
@@ -4,10 +4,15 @@ import com.codeborne.pdftest.PDF;
 import com.codeborne.xlstest.XLS;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 import static org.hamcrest.CoreMatchers.containsString;
@@ -22,6 +27,7 @@ public class SelenideFilesTest {
     public void txtTest() throws IOException {
         String txtFilePath = "./src/test/resources/1.txt";
         String expectedData = "hello qa.guru students!";
+
         String actualData = FileUtils.readFileToString(new File(txtFilePath), StandardCharsets.UTF_8);
         assertThat(actualData, containsString(expectedData));
     }
@@ -30,6 +36,7 @@ public class SelenideFilesTest {
     public void pdfTest() throws IOException {
         String pdfFilePath = "./src/test/resources/1.pdf";
         String expectedData = "JUnit 5 User Guide";
+
         PDF pdf = new PDF(new File(pdfFilePath));
         assertThat(pdf, PDF.containsText(expectedData));
     }
@@ -38,6 +45,7 @@ public class SelenideFilesTest {
     public void xlsTest() {
         String xlsFilePath = "./src/test/resources/1.xls";
         String expectedData = "hello qa.guru students!";
+
         XLS xls = new XLS(new File(xlsFilePath));
         String actualData = xls.excel.getSheetAt(0).getRow(3).getCell(1).toString();
         assertThat(actualData, containsString(expectedData));
@@ -52,7 +60,6 @@ public class SelenideFilesTest {
 
         ZipFile zipFile = new ZipFile(zipFilePath);
         zipFile.extractAll(unzipFolderPath);
-
         String actualData = FileUtils.readFileToString(new File(unzipTxtFilePath), StandardCharsets.UTF_8);
         assertThat(actualData, containsString(expectedData));
     }
@@ -70,8 +77,20 @@ public class SelenideFilesTest {
             zipFile.setPassword(password);
         }
         zipFile.extractAll(unzipFolderPath);
-
         String actualData = FileUtils.readFileToString(new File(unzipTxtFilePath), StandardCharsets.UTF_8);
+        assertThat(actualData, containsString(expectedData));
+    }
+
+        @Test
+    public void docxTest() throws IOException, InvalidFormatException {
+        String docFilePath = "1.docx";
+        String expectedData = "hello qa.guru students!";
+
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream stream = classLoader.getResourceAsStream(docFilePath);
+        XWPFDocument docx = new XWPFDocument(OPCPackage.open(stream));
+        XWPFWordExtractor extractor = new XWPFWordExtractor(docx);
+        String actualData = extractor.getText().trim();
         assertThat(actualData, containsString(expectedData));
     }
 }
